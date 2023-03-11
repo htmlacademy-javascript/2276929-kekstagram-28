f7c75528ec6d35c9b162abc6664d3d913c80ac52
AleksDamsky <aahripunov@mail.ru>
2023-03-11 21:08:13 +0400

    to make a function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 // Обратите внимание, в тексте задания вы будете встречать текст в фигурных скобках. Такой текст будет означать, что на месте этого текста должно появиться значение, которое вы возьмёте из данных. Например, в шаблоне может быть написано <div>{{x}}</div>, и это будет значить, что {{x}} нужно заменить на значение переменной x. Если переменная будет равна 100, то разметка должна выглядеть как <div>100</div>. Фигурные скобки в этой записи ничего не значат, они просто показывают, что закончилась разметка и в этом месте будут стоять данные. Сами фигурные скобки переносить в разметку не нужно.
-
 // В файле main.js напишите необходимые функции для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
-
 // Структура каждого объекта должна быть следующей:
-
 // id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
-
 // url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-
 // description, строка — описание фотографии. Описание придумайте самостоятельно.
-
 // likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-
 // comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение. Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
-
 // {
 //   id: 135,
 //   avatar: 'img/avatar-6.svg',
@@ -21,38 +13,13 @@
 //   name: 'Артём',
 // }
 // У каждого комментария есть идентификатор — id — любое число. Идентификаторы не должны повторяться.
-
 // Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img.
-
 // Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
 // Массив, откуда мы будем брать комментарии.
-// const strings = ['Всё отлично!',
-// "В целом всё неплохо. Но не всё.",
-// "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
-// "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
-// "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
-// "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!",
-// "Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.",
-// ];
-
-
 // Создам массив с объектом. Объект там один, как шаблон.
-
 // console.log(Object.values(objectDone));
-
-//Создаю функцию, которая генерит случайное число в диапазоне
-const getRandomNumber = function (a, b){
-  const up = Math.ceil(Math.min(a, b));
-  const low = Math.floor(Math.max(a, b));
-  const result = Math.random() * (up - low + 1) + low;
-
-  return Math.floor(result);
-};
-
-console.log(getRandomNumber(0, 15));
-
-//Здесь я создам массив с описанием фото
-const arrForDescription = [
+// -----------------------
+const ARRAYSDESCRIPTION = [
   'Далеко-далеко за словесными горами',
   'в стране гласных и согласных живут рыбные тексты',
   'Рукопись его единственное повстречался',
@@ -64,30 +31,94 @@ const arrForDescription = [
   'встретил переулка злых на берегу.',
   'Предложения!'
 ]
+// ---------------------
+const ARRAYSCOMMENTS = [
+'Всё отлично!',
+'в целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.',
+];
+//Создаю функцию, которая генерит случайное число в диапазоне
+const getRandomNumber = function (a, b){
+  const up = Math.ceil(Math.min(a, b));
+  const low = Math.floor(Math.max(a, b));
+  const result = Math.random() * (up - low + 1) + low;
 
-//Функция, которая будет перебирать массив и выводить случайный элемент.
-const getRandomDescription = function(arr){
-  let randomFrase = Math.floor(Math.random() * arr.length);
-
-  return arr[randomFrase];
-}
-
-console.log(getRandomDescription(arrForDescription));
-
-//Функция с переменными и объектом.
-const createMyObject = function() {
-  const randomId = getRandomNumber(0, 25);
-  const randomLikes = getRandomNumber(15, 250);
-  const randomDescription = getRandomDescription(arrForDescription);
-
-  let objectDone = [{
-    id: randomId,
-    url: '',
-    description: randomDescription,
-    likes: randomLikes,
-    comments: '',
-  }];
-
-  return objectDone;
+  return Math.floor(result);
 };
-console.log(createMyObject());
+
+const getRandomArrElem = function(array) {
+  array[getRandomNumber(0, array.length - 1)];
+};
+
+const getRandomId = function(){
+  let elementId = 0;
+
+  return () => {
+    elementId += 1;
+    return elementId;
+  };
+};
+
+const getRandomComment = (array) => {
+  Array.from({length: getRandomNumber(1, 2)}, () =>
+  getRandomArrElem(array).join(' ')
+  );
+};
+
+const createPicturesObject = (index) => ({
+  id: index,
+  comments: getRandomArrElem(ARRAYSCOMMENTS),
+  description: getRandomArrElem(ARRAYSDESCRIPTION),
+  likes: getRandomNumber(15, 250),
+  url: `photos/${index}.jpg`,
+});
+
+const getPictures = () => {
+  Array.from({length: 25}, (_, pictureIndex) => createPicturesObject(pictureIndex + 1));
+};
+
+console.log(getPictures());
+
+//Здесь я создам массив с описанием фото--------------------------------------------------------------------------------
+// //Функция, которая будет перебирать массив и выводить случайный элемент.
+// const getRandomDescription = function(arr){
+//   let randomFrase = Math.floor(Math.random() * arr.length);
+
+//   return arr[randomFrase];
+// };
+
+// //Делаем Рандомные комментарии.-------------------------------------------------------------------------------
+
+// //Функция, генерирующая случайные комментарии
+// const getRandomComment = function(arr) {
+//   let randomComment = Math.floor(Math.random() * arr.length);
+
+//   return arr[randomComment];
+// };
+
+// console.log(getRandomComment(comments));
+
+// //Функция с переменными и объектом.-------------------------------------------------------------------------------------
+// const createMyObject = function() {
+//   const randomId = getRandomNumber(0, 25);
+//   const randomLikes = getRandomNumber(15, 250);
+//   const randomDescription = getRandomDescription(arrForDescription);
+//   const randomComment = getRandomComment(comments);
+
+//   let objectDone = [{
+//     id: randomId,
+//     url: `photos/${getRandomNumber(1, 25)}.jpg`,
+//     description: randomDescription,
+//     likes: randomLikes,
+//     comments: randomComment,
+//   }];
+
+//   return objectDone;
+// };
+// console.log(createMyObject());
+
+// let myObject = Array.from({length: 25}, )
