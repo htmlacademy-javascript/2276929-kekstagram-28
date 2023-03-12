2a3de201b0b5f1c898203950be4a3fb3b74feacd
AleksDamsky <aahripunov@mail.ru>
2023-03-12 23:24:50 +0400

    try to made a 25 objects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Решение в самом конце
+
+
 // Обратите внимание, в тексте задания вы будете встречать текст в фигурных скобках. Такой текст будет означать, что на месте этого текста должно появиться значение, которое вы возьмёте из данных. Например, в шаблоне может быть написано <div>{{x}}</div>, и это будет значить, что {{x}} нужно заменить на значение переменной x. Если переменная будет равна 100, то разметка должна выглядеть как <div>100</div>. Фигурные скобки в этой записи ничего не значат, они просто показывают, что закончилась разметка и в этом месте будут стоять данные. Сами фигурные скобки переносить в разметку не нужно.
 // В файле main.js напишите необходимые функции для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
 // Структура каждого объекта должна быть следующей:
@@ -19,69 +22,69 @@
 // Создам массив с объектом. Объект там один, как шаблон.
 // console.log(Object.values(objectDone));
 // -----------------------
-const ARRAYSDESCRIPTION = [
-  'Далеко-далеко за словесными горами',
-  'в стране гласных и согласных живут рыбные тексты',
-  'Рукопись его единственное повстречался',
-  'бросил имени предупреждал путь ручеек ему власти домах гор лучше!',
-  'Безопасную лучше моей океана не своих.',
-  'Родного там океана парадигматическая',
-  'маленький домах текстами инициал знаках запятой',
-  'сбить переписывается заманивший выйти сих,',
-  'встретил переулка злых на берегу.',
-  'Предложения!'
-]
-// ---------------------
-const ARRAYSCOMMENTS = [
-'Всё отлично!',
-'в целом всё неплохо. Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.',
-];
-//Создаю функцию, которая генерит случайное число в диапазоне
-const getRandomNumber = function (a, b){
-  const up = Math.ceil(Math.min(a, b));
-  const low = Math.floor(Math.max(a, b));
-  const result = Math.random() * (up - low + 1) + low;
+// const ARRAYSDESCRIPTION = [
+//   'Далеко-далеко за словесными горами',
+//   'в стране гласных и согласных живут рыбные тексты',
+//   'Рукопись его единственное повстречался',
+//   'бросил имени предупреждал путь ручеек ему власти домах гор лучше!',
+//   'Безопасную лучше моей океана не своих.',
+//   'Родного там океана парадигматическая',
+//   'маленький домах текстами инициал знаках запятой',
+//   'сбить переписывается заманивший выйти сих,',
+//   'встретил переулка злых на берегу.',
+//   'Предложения!'
+// ]
+// // ---------------------
+// const ARRAYSCOMMENTS = [
+// 'Всё отлично!',
+// 'в целом всё неплохо. Но не всё.',
+// 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+// 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+// 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+// 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+// 'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.',
+// ];
+// //Создаю функцию, которая генерит случайное число в диапазоне
+// const getRandomNumber = function (a, b){
+//   const up = Math.ceil(Math.min(a, b));
+//   const low = Math.floor(Math.max(a, b));
+//   const result = Math.random() * (up - low + 1) + low;
 
-  return Math.floor(result);
-};
+//   return Math.floor(result);
+// };
 
-const getRandomArrElem = function(array) {
-  array[getRandomNumber(0, array.length - 1)];
-};
+// const getRandomArrElem = function(array) {
+//   array[getRandomNumber(0, array.length - 1)];
+// };
 
-const getRandomId = function(){
-  let elementId = 0;
+// const getRandomId = function(){
+//   let elementId = 0;
 
-  return () => {
-    elementId += 1;
-    return elementId;
-  };
-};
+//   return () => {
+//     elementId += 1;
+//     return elementId;
+//   };
+// };
 
-const getRandomComment = (array) => {
-  Array.from({length: getRandomNumber(1, 2)}, () =>
-  getRandomArrElem(array).join(' ')
-  );
-};
+// const getRandomComment = (array) => {
+//   Array.from({length: getRandomNumber(1, 2)}, () =>
+//   getRandomArrElem(array).join(' ')
+//   );
+// };
 
-const createPicturesObject = (index) => ({
-  id: index,
-  comments: getRandomArrElem(ARRAYSCOMMENTS),
-  description: getRandomArrElem(ARRAYSDESCRIPTION),
-  likes: getRandomNumber(15, 250),
-  url: `photos/${index}.jpg`,
-});
+// const createPicturesObject = (index) => ({
+//   id: index,
+//   comments: getRandomArrElem(ARRAYSCOMMENTS),
+//   description: getRandomArrElem(ARRAYSDESCRIPTION),
+//   likes: getRandomNumber(15, 250),
+//   url: `photos/${index}.jpg`,
+// });
 
-const getPictures = () => {
-  Array.from({length: 25}, (_, pictureIndex) => createPicturesObject(pictureIndex + 1));
-};
+// const getPictures = () => {
+//   Array.from({length: 25}, (_, pictureIndex) => createPicturesObject(pictureIndex + 1));
+// };
 
-console.log(getPictures());
+// console.log(getPictures());
 
 //Здесь я создам массив с описанием фото--------------------------------------------------------------------------------
 // //Функция, которая будет перебирать массив и выводить случайный элемент.
@@ -122,3 +125,91 @@ console.log(getPictures());
 // console.log(createMyObject());
 
 // let myObject = Array.from({length: 25}, )
+
+
+// Вот решение
+
+const DESCRIPTION = [
+  'Далеко-далеко за словесными горами',
+  'в стране гласных и согласных живут рыбные тексты',
+  'Рукопись его единственное повстречался',
+  'бросил имени предупреждал путь ручеек ему власти домах гор лучше!',
+  'Безопасную лучше моей океана не своих.',
+  'Родного там океана парадигматическая',
+  'маленький домах текстами инициал знаках запятой',
+  'сбить переписывается заманивший выйти сих,',
+  'встретил переулка злых на берегу.',
+  'Предложения!'
+];
+
+const ALLCOMMENTS = [
+  'Всё отлично!',
+  'в целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.',
+];
+
+const USERNAME = [
+  "Artem",
+  "Irina",
+  "Sergey",
+  "Boris",
+  "Aleks",
+  "BigHead",
+  "Gilfoyl",
+  "Gevin",
+  "Erlik",
+  "Dinesh",
+];
+
+const getRandomNumber = function (a, b){
+  const up = Math.ceil(Math.min(a, b));
+  const low = Math.floor(Math.max(a, b));
+  const result = Math.random() * (up - low + 1) + low;
+
+  return Math.floor(result);
+};
+
+let allPhotos = [];
+
+const createComment = (index) => {
+  let comments = [];
+  for(let i = 0; i <= 25; i++){
+    let getRandomAva = getRandomNumber(0, 6);
+    let getRandomComment = getRandomNumber(0, ALLCOMMENTS.length - 1);
+    let getRandomName = getRandomNumber(0, USERNAME.length - 1);
+    comments.push({
+      commentId: +(String(index) + String(i)),
+      avatar: `img/avatar-${getRandomAva}.svg`,
+      message: ALLCOMMENTS[getRandomComment],
+      name: USERNAME[getRandomName],
+    });
+
+  }
+  return comments;
+};
+
+
+
+
+const createUsers = () => {
+  let getRandomLikes = getRandomNumber(15, 250);
+
+  for(let i = 1; i <= 25; i++){
+    let photo = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomNumber(0, DESCRIPTION.length - 1),
+      likes: getRandomLikes,
+      comments: createComment(i),
+    };
+    allPhotos.push(photo);
+  }
+  return allPhotos;
+};
+
+createUsers();
+console.log(allPhotos);
